d0d3ba856b6fa5fdef1f294aa240979fd454d402
Antonov Dmitriy <antonovdi@inversion.ru>
2016-06-01 11:49:12 +0300

    first

--- src/main/java/com/antonov/service/NAAService.java
+++ src/main/java/com/antonov/service/NAAService.java
@@ -11,30 +11,12 @@ import javax.ws.rs.core.MediaType;
 /**
  * Created by dmitriyantonov on 31.05.16.
  */
-// The Java class will be hosted at the URI path "/helloworld"
 @Path("/helloworld")
 public class NAAService {
-    // The Java method will process HTTP GET requests
-    @GET
 
-    // The Java method will produce content identified by the MIME Media type "text/plain"
-//    @Produces(MediaType.TEXT_XML)
+    @GET
     @Produces("text/plain")
     public String getClichedMessage() {
-        // Return some cliched textual content
         return "Hello World";
     }
-//
-//    public static void main(String[] args) throws IOException {
-//        HttpServer server = HttpServerFactory.create("http://localhost:9998/");
-//        server.start();
-//
-//        System.out.println("Server running");
-//        System.out.println("Visit: http://localhost:9998/helloworld");
-//        System.out.println("Hit return to stop...");
-//        System.in.read();
-//        System.out.println("Stopping server");
-//        server.stop(0);
-//        System.out.println("Server stopped");
-//    }
 }
